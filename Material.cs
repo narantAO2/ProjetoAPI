3c3a4549c20bbfd172795e9923d0ac1eeedcc75d
Aleff2804 <aleff.fernandes2804@gmail.com>
2025-11-16 19:21:59 -0300

    Delete Material.cs

--- Material.cs
+++
@@ -1,9 +0,0 @@
-namespace OdontoApi.Models
-{
-    public class Material
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public int Quantity { get; set; }
-    }
-}
