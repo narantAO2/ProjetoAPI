7fa574816efea928aebea511a759c4bc95afc56d
narantAO2 <jubileunaranti@gmail.com>
2025-11-18 09:23:29 -0300

    Update AppointmentsController.cs

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -5,53 +5,29 @@ using OdontoApi.Models;
 
 namespace OdontoApi.Controllers
 {
-    // Controller responsável pelos endpoints da API para trabalhar com Appointment (consultas)
-    // A rota base será: http://localhost:5083/Appointments
     [ApiController]
     [Route("Appointments")]
     public class AppointmentsController : ControllerBase
     {
-        // Injeção de dependência do AppDbContext para acessar o banco de dados
         private readonly AppDbContext _db;
         public AppointmentsController(AppDbContext db) => _db = db;
-
-        // GET /Appointments
-        // Busca TODAS as consultas no banco, incluindo os dados relacionados de Dentist e Procedure
+        
         [HttpGet]
         public async Task<IEnumerable<Appointment>> Get() =>
             await _db.Appointments
-                .Include(a => a.Dentist)   // faz o join para trazer o dentista
-                .Include(a => a.Procedure) // faz o join para trazer o procedimento
+                .Include(a => a.Dentist)
+                .Include(a => a.Procedure)   // faz o join para trazer os parametros 
                  .Include(a => a.Material)
                 .ToListAsync();
-
-        // GET /Appointments/{id}
-        // Busca UMA consulta específica pelo Id; retorna 404 se não encontrar
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Appointment>> Get(int id)
-        {
-            var a = await _db.Appointments
-                .Include(x => x.Dentist)    // inclui o dentista na consulta
-                .Include(x => x.Procedure)  // inclui o procedimento na consulta
-                .Include(x => x.Material)
-                .FirstOrDefaultAsync(x => x.Id == id); // filtra pelo Id recebido
-
-            return a == null ? NotFound() : a;
-        }
-
-        // POST /Appointments
-        // Cria uma nova consulta (Appointment) no banco de dados
-        // Antes de salvar, valida se o DentistId e o ProcedureId existem
+                
         [HttpPost]
         public async Task<ActionResult<Appointment>> Post(Appointment a)
         {
-            // Valida se existe um dentista com o Id informado
             if (!_db.Dentists.Any(d => d.Id == a.DentistId))
                 return BadRequest("DentistId inválido.");
-
-            // Valida se existe um procedimento com o Id informado
+                
             if (!_db.Procedures.Any(p => p.Id == a.ProcedureId))
-                return BadRequest("ProcedureId inválido.");
+                return BadRequest("ProcedureId inválido.");          // Valida se existe algo com o Id informado
 
             if (!_db.Materials.Any(m => m.Id == a.MaterialId))
                 return BadRequest("MaterialId inválido.");
